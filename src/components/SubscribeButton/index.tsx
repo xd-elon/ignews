95bd1a9b770653927384c46fa3a9e2710c2b4a77
xd-elon <alandevelope@gmail.com>
2021-07-24 19:06:21 -0300

    ssr, stripe, api, service

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscriptionButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscriptionButtonProps) {
     return (
         <button
             type="button"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
   return (
     <>
       <Head>
@@ -13,16 +24,37 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.here}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span>World.</h1>
+          <h1>News about the <span>React </span>World.</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for $9.90</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1JGYbVHaG7xIWyAD0qfAXxHF', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount/100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
